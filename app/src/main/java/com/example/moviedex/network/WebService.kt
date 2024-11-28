1428d09ef01196696ea8790299cdeaf52027634f
NicoXtroid <nicole.riova@gmail.com>
2024-11-28 02:21:33 -0300

    Data layer

---
+++ app/src/main/java/com/example/moviedex/network/WebService.kt
@@ -0,0 +1,22 @@
+package com.example.moviedex.network
+
+import com.example.moviedex.network.response.ResponseMovieList
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WebService {
+    @GET("popular")
+    suspend fun getPopularMovies(
+        @Query("api_key") apiKey: String
+    ): ResponseMovieList
+
+    @GET("top_rated")
+    suspend fun getTopRatedMovies(
+        @Query("api_key") apiKey: String
+    ): ResponseMovieList
+
+    @GET("upcoming")
+    suspend fun getUpcomingMovies(
+        @Query("api_key") apiKey: String
+    ): ResponseMovieList
+}
